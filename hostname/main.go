807cbe739429cf07dcb64fc1f5b06979bc86ccee
David Lavieri <daviddlavier@gmail.com>
2025-05-24 16:28:48 +0200

    clean up go code

--- hostname/main.go
+++ hostname/main.go
@@ -11,27 +11,29 @@ import (
 	"os/signal"
 )
 
-var hostname string
-var logSourceIp *bool = new(bool)
+var (
+	hostname    string
+	logSourceIp bool
+)
 
+// Handlers
 func handleHostname(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
-	var attrs = make([]any, 0, 8)
-	attrs = append(attrs, "hostname", hostname, "path", r.URL.Path)
-
-	if *logSourceIp {
+	attrs := []any{
+		"hostname", hostname, "path", r.URL.Path,
+	}
+	if logSourceIp {
 		attrs = append(attrs, "ip", r.RemoteAddr, "forwarded-for", r.Header.Get("X-Forwarded-For"))
 	}
-
 	slog.Info("hostname request", attrs...)
 
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
-	_, err := w.Write([]byte(hostname))
-	if err != nil {
+	if _, err := w.Write([]byte(hostname)); err != nil {
 		log.Println(err)
 	}
 }
+
 func handleHealth(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	w.Header().Set("Content-Type", "text/plain")
@@ -46,73 +48,18 @@ func handleHealth(w http.ResponseWriter, r *http.Request) {
 	}
 
 	slog.Info("healthcheck request", "status", msg)
-
-	_, err := w.Write([]byte(msg))
-	if err != nil {
+	if _, err := w.Write([]byte(msg)); err != nil {
 		log.Println(err)
 	}
 }
 
 func handleNotFound(w http.ResponseWriter, _ *http.Request) {
-	w.WriteHeader(404)
+	w.WriteHeader(http.StatusNotFound)
 }
 
-func main() {
-	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
-	defer cancel()
-
-	// try find it on ENV
-	*logSourceIp = false
-	hostname = os.Getenv("HOSTNAME")
-	var err error
-
-	if os.Getenv("LOG_SOURCE_IP") != "" && os.Getenv("LOG_SOURCE_IP") != "0" && os.Getenv("LOG_SOURCE_IP") != "false" {
-		*logSourceIp = true
-	}
-
-	// read hostname from file
-	if hostname == "" {
-		if hostname, err = fromFile("/etc/hostname"); err != nil {
-			slog.Error("unable to read hostname from /etc/hostname", "error", err)
-			os.Exit(1)
-		}
-	}
-
-	// setup server
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "80"
-	}
-
-	server := &http.Server{
-		Addr: fmt.Sprintf(":%s", port),
-	}
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /", handleHostname)
-	mux.HandleFunc("GET /hostname", handleHostname)
-	mux.HandleFunc("GET /healthz", handleHealth)
-	mux.HandleFunc("GET /{catchall}", handleNotFound)
-
-	server.Handler = mux
-
-	go func() {
-		<-ctx.Done()
-		log.Println("shutting down server")
-		err := server.Shutdown(ctx)
-		if err != nil {
-			log.Println("error shutting down http server:", err)
-		}
-	}()
-
-	slog.Info("http server started", "port", port)
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatalln(err)
-	}
-}
-
-func fromFile(p string) (string, error) {
-	f, err := os.Open(p)
+// Utility function to read hostname from file
+func fromFile(path string) (string, error) {
+	f, err := os.Open(path)
 	if err != nil {
 		return "", err
 	}
@@ -122,7 +69,60 @@ func fromFile(p string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	hostname = string(b)
+	return string(b), nil
+}
 
-	return hostname, nil
+// Setup configuration from environment variables/files
+func initConfig() {
+	logSourceIp = os.Getenv("LOG_SOURCE_IP") != "" &&
+		os.Getenv("LOG_SOURCE_IP") != "0" &&
+		os.Getenv("LOG_SOURCE_IP") != "false"
+
+	hostname = os.Getenv("HOSTNAME")
+	if hostname == "" {
+		var err error
+		if hostname, err = fromFile("/etc/hostname"); err != nil {
+			slog.Error("unable to read hostname from /etc/hostname", "error", err)
+			os.Exit(1)
+		}
+	}
+}
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer cancel()
+
+	initConfig()
+
+	// Create server and handlers
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+	addr := fmt.Sprintf(":%s", port)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /", handleHostname)
+	mux.HandleFunc("GET /hostname", handleHostname)
+	mux.HandleFunc("GET /healthz", handleHealth)
+	mux.HandleFunc("GET /{catchall}", handleNotFound)
+
+	server := &http.Server{
+		Addr:    addr,
+		Handler: mux,
+	}
+
+	// Context for graceful shutdown
+	go func() {
+		<-ctx.Done()
+		log.Println("shutting down server")
+		if err := server.Shutdown(ctx); err != nil {
+			log.Println("error shutting down http server:", err)
+		}
+	}()
+
+	slog.Info("http server started", "port", port)
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalln(err)
+	}
 }
